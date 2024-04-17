6f1cddf1581fa536be752bdb9039e2d8885e5643
PallaviPokuri <pokuripallavi2002@gmail.com>
2024-04-17 17:50:09 -0500

    update

--- models/Icecream.js
+++ models/Icecream.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 const IcecreamSchema = mongoose.Schema({
-Icecream_flavour: String,
+Icecream_flavour: {type:String,length:15},
 Icecream_toppings: String,
-Icecream_price: Number,
+Icecream_price: {type:Number,min:10,max:50}
 })
 module.exports = mongoose.model("Icecream",IcecreamSchema)
